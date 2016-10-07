ea6fd02f7b53d80eef732088ec780151c2469e0c
ksatake <k.satake.com@gmail.com>
2016-10-07 20:00:36 +0900

    program finished

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,13 @@
 # Hello World!
 class HelloTrema < Trema::Controller
   def start(_args)
-    logger.info 'Trema started.'
+    logger.info self.name+' started.'
   end
 
   def switch_ready(datapath_id)
     logger.info "Hello #{datapath_id.to_hex}!"
+  end
+  def switch_disconnected(datapath_id)
+    logger.info "Bye #{datapath_id.to_hex}"
   end
 end
